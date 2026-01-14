50d11f09e140c4d985689480c3c724e850ccfe04
aymenkrifa <aymenkrifa@gmail.com>
2026-01-14 18:16:32 +0100

    Update website footer to 2026!

--- app/footer.tsx
+++ app/footer.tsx
@@ -72,7 +72,7 @@ export function Footer() {
       <div className="flex items-center justify-between">
         <a href="https://github.com/ibelick/nim" target="_blank">
           <TextLoop className="text-xs text-zinc-500">
-            <span>© 2025 Aymen Krifa</span>
+            <span>© 2026 Aymen Krifa</span>
             <span>Template inspired by Nim</span>
           </TextLoop>
         </a>
